474dc5b1135e16462d8e9d0df4f5509901eeb817
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-02-06 16:29:04 -0700

    got query link working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
 import Login from './components/Login';
+import Queries from './components/Questions/Queries';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        {/* <Route path="/Queries" element={<Queries />} /> */}
+        <Route path="/Queries" element={<Queries />} />
         <Route path="/Login" element={<Login />} />
         {/* Define other routes as needed */}
       </Routes>
--- client/src/components/Questions/Queries.jsx
+++ client/src/components/Questions/Queries.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { questions } from './Question.js';
+// import { questions } from './Question.js';
 
 
 function Queries() {
@@ -13,34 +13,3 @@ function Queries() {
   }
   
   export default Queries;
-
-
-
-
-/*
-// DOM elements
-const questionText = document.getElementById('question-text');
-const nextButton = document.getElementById('next-button');
-
-let currentQuestionIndex = -1;
-
-// Function to display a random question
-function displayRandomQuestion() {
-    // Generate a random index within the range of your questions array
-    const randomIndex = Math.floor(Math.random() * questions.length);
-
-    // Ensure we don't repeat the same question
-    if (randomIndex !== currentQuestionIndex) {
-        currentQuestionIndex = randomIndex;
-        questionText.textContent = questions[randomIndex];
-    } else {
-        // If the random question is the same as the previous one, call the function again
-        displayRandomQuestion();
-    }
-}
-
-// Event listener for the "Next Question" button
-nextButton.addEventListener('click', displayRandomQuestion);
-
-// Initial display of the first random question
-displayRandomQuestion();g
