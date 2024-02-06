6fe4f6f51f7dd6065cd3cf78b600ad6998fd9baf
SlemJosh <joshua.slem@gmail.com>
2024-02-05 19:08:07 -0700

    Adding Questions for the Question.js

---
+++ client/src/components/Question.js
@@ -0,0 +1,114 @@
+[
+    {
+        "id": 1,
+        "question": "What is the occasion?",
+        "answers": {
+            "Birthday": "Birthday",
+            "Valentines": "Valentine",
+            "Easter": "Easter",
+            "Mothers Day": "Mother",
+            "Fathers Day": "Father",
+            "Christmas": "Christmas",
+            "Halloween": "Halloween",
+            "Other": "User-Defined"
+        }
+    },
+    {
+        "id": 2,
+        "question": "What’s it for?",
+        "answers": {
+            "Wearing": "Apparel",
+            "Eating": "Gourmet",
+            "Playing With": "Toy",
+            "Watching": "Entertainment",
+            "Other": "User-Defined"
+        }
+    },
+    {
+        "id": 3,
+        "question": "Is this for a romantic partner?",
+        "answers": {
+            "Yes": "Romantic",
+            "No": "General"
+        }
+    },
+    {
+        "id": 4,
+        "question": "Is this for a child?",
+        "answers": {
+            "Yes": "Child",
+            "No": "Adult"
+        }
+    },
+    {
+        "id": 5,
+        "question": "What is the recipient's favorite color?",
+        "answers": {
+            "Blue": "Blue",
+            "Green": "Green",
+            "Purple": "Purple",
+            "Red": "Red",
+            "Orange": "Orange",
+            "Other": "User-Defined"
+        }
+    },
+    {
+        "id": 6,
+        "question": "What’s your price range for this gift?",
+        "answers": {
+            "High": "Luxury",
+            "Mid-range": "Midrange",
+            "Low": "Budget"
+        }
+    },
+    {
+        "id": 7,
+        "question": "What room is this gift for?",
+        "answers": {
+            "Kitchen": "Kitchen",
+            "Bathroom": "Bathroom",
+            "Living/Family Room": "Living",
+            "Bedroom": "Bedroom",
+            "Garage": "Garage",
+            "Laundry Room": "Laundry",
+            "Other": "User-Defined"
+        }
+    },
+    {
+        "id": 8,
+        "question": "Does the recipient enjoy any of the following genres?",
+        "answers": {
+            "Fantasy": "Fantasy",
+            "Sci Fi": "SciFi",
+            "True Crime": "Crime",
+            "Mystery": "Mystery",
+            "Romance": "Romance",
+            "Historical Fiction": "Historical",
+            "Horror": "Horror",
+            "Thriller": "Thriller",
+            "Adventure": "Adventure",
+            "Biography": "Biography"
+        }
+    },
+    {
+        "id": 9,
+        "question": "Which of these hobbies or interests best describes the gift recipient?",
+        "answers": {
+            "Gardening": "Gardening",
+            "Technology": "Tech",
+            "Cooking": "Cooking",
+            "Sports": "Sports"
+        }
+    },
+    {
+        "id": 10,
+        "question": "What type of entertainment does the gift recipient enjoy the most?",
+        "answers": {
+            "Music": "Music",
+            "Books": "Books",
+            "Movies": "Movies",
+            "TV Shows": "TV",
+            "Video Games": "Gaming"
+        }
+    }
+]
