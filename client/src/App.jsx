b3b5620e849daf5e9588a1bd162bf5e68457ad8e
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-02-06 17:16:04 -0700

    added signup component, adding links from login to signup and welcome and  from signup to login and  welcome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
 import Login from './components/Login';
 import Queries from './components/Questions/Queries';
+import Signup from './components/SignUp';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<WelcomePage />} />
         <Route path="/Queries" element={<Queries />} />
         <Route path="/Login" element={<Login />} />
+        <Route path='/Signup' element={<Signup />} />
         {/* Define other routes as needed */}
       </Routes>
     </Router>
