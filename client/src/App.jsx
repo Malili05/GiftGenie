274f0f452f442aa8acd7d82d1056d16e0f5b22be
ProgramerNinja <nohamcmjt@gmail.com>
2024-02-07 20:33:53 -0700

    added some queries and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
-import WelcomePage from './components/WelcomePage';
-import Login from './components//Login/Login';
-import Search from './components/Search/Search';
-import Signup from './components/SignUp';
-import Results from './components/Results/Results';
-import Profile from './components/Profile/Profile';
+import { Outlet } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -30,17 +29,7 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/Search" element={<Search />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path='/Signup' element={<Signup />} />
-        <Route path='/Results' element={<Results />} />
-        <Route path='/Profile' element={<Profile />} />
-        {/* Define other routes as needed */}
-      </Routes>
-    </Router>
+          <Outlet />
     </ApolloProvider>
   );
 };
