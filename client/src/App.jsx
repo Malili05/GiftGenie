6407d3d622cc193443b829d0bb878a4feeeb68a8
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-02-07 20:17:26 -0700

    stuff we just did with rob

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
 import WelcomePage from './components/WelcomePage';
 import Login from './components//Login/Login';
 import Search from './components/Search/Search';
@@ -7,8 +8,28 @@ import Signup from './components/SignUp';
 import Results from './components/Results/Results';
 import Profile from './components/Profile/Profile';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
@@ -20,6 +41,7 @@ const App = () => {
         {/* Define other routes as needed */}
       </Routes>
     </Router>
+    </ApolloProvider>
   );
 };
 
