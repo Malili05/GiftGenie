e416949fe24e58ad35b879c31a94a1a2c98df9b9
ProgramerNinja <nohamcmjt@gmail.com>
2024-02-12 19:58:41 -0700

    rando fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    watch: false,
+  },
   server: {
     port: 3000,
     open: true,
